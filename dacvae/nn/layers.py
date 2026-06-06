0fed23b8e07cf01d76252688af984ed485f3fbe4
Cihan Yalçın <mcihann19@hotmail.com>
2026-06-07 00:38:28 +0300

    Add initial implementation of DACVAE model components

---
+++ dacvae/nn/layers.py
@@ -0,0 +1,12 @@
+import math
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+@torch.jit.script
+def snake(x, alpha):
+    shape = x.shape
+    x = x.reshape(shape[0], shape[1], -1)
+    x = x + (alpha + 1e-9).reciprocal() * torch.sin(alpha * x).pow(2)
+    x = x.reshape(shape)
+    return x
